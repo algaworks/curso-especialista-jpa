ee8983e7adff8b03e3d40b254b031230563b15a2
AlsGomes <als_08.net@hotmail.com>
2023-05-15 17:24:52 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 02.09-inserindo-o-primeiro-objeto-com-persist/src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ 02.09-inserindo-o-primeiro-objeto-com-persist/src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -2,8 +2,8 @@ package com.algaworks.ecommerce.iniciandocomjpa;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Produto;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
@@ -25,7 +25,7 @@ public class OperacoesComTransacaoTest extends EntityManagerTest {
         entityManager.clear();
 
         Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
-        Assert.assertNotNull(produtoVerificacao);
+        Assertions.assertNotNull(produtoVerificacao);
     }
 
     @Test
