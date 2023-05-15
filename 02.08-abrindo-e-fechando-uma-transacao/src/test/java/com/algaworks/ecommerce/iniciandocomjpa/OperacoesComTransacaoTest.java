ea50d2aef20582aba4b28c8a57fb450b7703df4d
AlsGomes <als_08.net@hotmail.com>
2023-05-15 17:15:50 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 02.08-abrindo-e-fechando-uma-transacao/src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ 02.08-abrindo-e-fechando-uma-transacao/src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -2,7 +2,7 @@ package com.algaworks.ecommerce.iniciandocomjpa;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Produto;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
