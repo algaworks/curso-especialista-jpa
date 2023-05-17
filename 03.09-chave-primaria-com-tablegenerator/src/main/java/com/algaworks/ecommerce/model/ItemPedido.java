22d09a8431f9dc3f2e67bbf934aebaffc32826a9
AlsGomes <als_08.net@hotmail.com>
2023-05-17 19:41:02 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 03.09-chave-primaria-com-tablegenerator/src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
+++ 03.09-chave-primaria-com-tablegenerator/src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
@@ -4,10 +4,10 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 
 @Getter
