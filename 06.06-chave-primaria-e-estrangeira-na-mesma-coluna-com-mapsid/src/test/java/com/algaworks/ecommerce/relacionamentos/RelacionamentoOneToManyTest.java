0d2ada1870ef255eeb05f06abb16c95d50195ef3
AlsGomes <als_08.net@hotmail.com>
2023-05-19 20:39:38 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 06.06-chave-primaria-e-estrangeira-na-mesma-coluna-com-mapsid/src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoOneToManyTest.java
+++ 06.06-chave-primaria-e-estrangeira-na-mesma-coluna-com-mapsid/src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoOneToManyTest.java
@@ -2,8 +2,8 @@ package com.algaworks.ecommerce.relacionamentos;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.*;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -28,11 +28,13 @@ public class RelacionamentoOneToManyTest extends EntityManagerTest {
         entityManager.clear();
 
         Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
-        Assert.assertFalse(clienteVerificacao.getPedidos().isEmpty());
+        Assertions.assertFalse(clienteVerificacao.getPedidos().isEmpty());
     }
 
     @Test
     public void verificarRelacionamentoPedido() {
+        entityManager.getTransaction().begin();
+
         Cliente cliente = entityManager.find(Cliente.class, 1);
         Produto produto = entityManager.find(Produto.class, 1);
 
@@ -42,20 +44,28 @@ public class RelacionamentoOneToManyTest extends EntityManagerTest {
         pedido.setTotal(BigDecimal.TEN);
         pedido.setCliente(cliente);
 
+        entityManager.persist(pedido);
+
+        // Pode ser que logo ao executar o método "persist" o JPA já faça a sincronização com a base.
+        // Mas caso isso não aconteça, o flush garante a sincronização.
+        entityManager.flush();
+
         ItemPedido itemPedido = new ItemPedido();
+//        itemPedido.setPedidoId(pedido.getId());
+//        itemPedido.setProdutoId(produto.getId());
+        itemPedido.setId(new ItemPedidoId(pedido.getId(), produto.getId()));
         itemPedido.setPrecoProduto(produto.getPreco());
         itemPedido.setQuantidade(1);
         itemPedido.setPedido(pedido);
         itemPedido.setProduto(produto);
 
-        entityManager.getTransaction().begin();
-        entityManager.persist(pedido);
         entityManager.persist(itemPedido);
+
         entityManager.getTransaction().commit();
 
         entityManager.clear();
 
         Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
-        Assert.assertFalse(pedidoVerificacao.getItens().isEmpty());
+        Assertions.assertFalse(pedidoVerificacao.getItens().isEmpty());
     }
 }
