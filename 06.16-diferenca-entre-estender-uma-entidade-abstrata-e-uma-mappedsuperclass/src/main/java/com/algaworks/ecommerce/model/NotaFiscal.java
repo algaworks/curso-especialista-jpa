4861e9d46b9cd9b76e90b6dd8d3d9dabce711fc7
AlsGomes <als_08.net@hotmail.com>
2023-05-22 20:07:00 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 06.16-diferenca-entre-estender-uma-entidade-abstrata-e-uma-mappedsuperclass/src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
+++ 06.16-diferenca-entre-estender-uma-entidade-abstrata-e-uma-mappedsuperclass/src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
@@ -4,7 +4,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.util.Date;
 
@@ -23,6 +23,7 @@ public class NotaFiscal extends EntidadeBaseInteger {
     private Pedido pedido;
 
     @Lob
+	@Column(length = 1000)
     private byte[] xml;
 
     @Column(name = "data_emissao")
