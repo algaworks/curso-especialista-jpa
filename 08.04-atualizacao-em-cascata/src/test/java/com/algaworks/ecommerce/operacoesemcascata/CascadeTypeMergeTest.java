75f12ad0eaccd05e141d7616bb0069b78f2cb2e9
AlsGomes <als_08.net@hotmail.com>
2023-05-26 18:58:07 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 08.04-atualizacao-em-cascata/src/test/java/com/algaworks/ecommerce/operacoesemcascata/CascadeTypeMergeTest.java
+++ 08.04-atualizacao-em-cascata/src/test/java/com/algaworks/ecommerce/operacoesemcascata/CascadeTypeMergeTest.java
@@ -2,8 +2,8 @@ package com.algaworks.ecommerce.operacoesemcascata;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.*;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -40,7 +40,7 @@ public class CascadeTypeMergeTest extends EntityManagerTest {
         entityManager.clear();
 
         ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
-        Assert.assertTrue(itemPedidoVerificacao.getQuantidade().equals(3));
+        Assertions.assertTrue(itemPedidoVerificacao.getQuantidade().equals(3));
     }
 
     // @Test
@@ -71,6 +71,6 @@ public class CascadeTypeMergeTest extends EntityManagerTest {
         entityManager.clear();
 
         ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
-        Assert.assertTrue(StatusPedido.PAGO.equals(itemPedidoVerificacao.getPedido().getStatus()));
+        Assertions.assertTrue(StatusPedido.PAGO.equals(itemPedidoVerificacao.getPedido().getStatus()));
     }
 }
