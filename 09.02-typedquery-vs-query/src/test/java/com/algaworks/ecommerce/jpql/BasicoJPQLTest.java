f2f801aadf7dfd9ebf866e72f1a3e0e4acdaa8ff
AlsGomes <als_08.net@hotmail.com>
2023-05-26 19:29:21 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 09.02-typedquery-vs-query/src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
+++ 09.02-typedquery-vs-query/src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
@@ -2,11 +2,11 @@ package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Pedido;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 import java.util.List;
 
 public class BasicoJPQLTest extends EntityManagerTest {
@@ -19,10 +19,10 @@ public class BasicoJPQLTest extends EntityManagerTest {
                 .createQuery("select p from Pedido p where p.id = 1", Pedido.class);
 
         Pedido pedido = typedQuery.getSingleResult();
-        Assert.assertNotNull(pedido);
+        Assertions.assertNotNull(pedido);
 
 //        List<Pedido> lista = typedQuery.getResultList();
-//        Assert.assertFalse(lista.isEmpty());
+//        Assertions.assertFalse(lista.isEmpty());
     }
 
     @Test
@@ -31,13 +31,13 @@ public class BasicoJPQLTest extends EntityManagerTest {
 
         TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
         Pedido pedido1 = typedQuery.getSingleResult();
-        Assert.assertNotNull(pedido1);
+        Assertions.assertNotNull(pedido1);
 
         Query query = entityManager.createQuery(jpql);
         Pedido pedido2 = (Pedido) query.getSingleResult();
-        Assert.assertNotNull(pedido2);
+        Assertions.assertNotNull(pedido2);
 
 //        List<Pedido> lista = query.getResultList();
-//        Assert.assertFalse(lista.isEmpty());
+//        Assertions.assertFalse(lista.isEmpty());
     }
 }
