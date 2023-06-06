39304519ad7823c62581c031e3e771eab230a0e0
AlsGomes <als_08.net@hotmail.com>
2023-06-06 19:23:55 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 09.17-expressao-diferente/src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ 09.17-expressao-diferente/src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -5,11 +5,11 @@ import com.algaworks.ecommerce.model.NotaFiscal;
 import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.Produto;
 import com.algaworks.ecommerce.model.StatusPagamento;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import javax.persistence.TemporalType;
-import javax.persistence.TypedQuery;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.TypedQuery;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -24,7 +24,7 @@ public class ExpressoesCondicionaisTest extends EntityManagerTest {
         TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
 
         List<Produto> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
     }
 
     @Test
@@ -37,7 +37,7 @@ public class ExpressoesCondicionaisTest extends EntityManagerTest {
         typedQuery.setParameter("dataFinal", LocalDateTime.now());
 
         List<Pedido> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
     }
 
     @Test
@@ -48,7 +48,7 @@ public class ExpressoesCondicionaisTest extends EntityManagerTest {
         typedQuery.setParameter("data", LocalDateTime.now().minusDays(2));
 
         List<Pedido> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
     }
 
     @Test
@@ -61,7 +61,7 @@ public class ExpressoesCondicionaisTest extends EntityManagerTest {
         typedQuery.setParameter("precoFinal", new BigDecimal(1500));
 
         List<Produto> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
     }
 
     @Test
@@ -71,7 +71,7 @@ public class ExpressoesCondicionaisTest extends EntityManagerTest {
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
         List<Object[]> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
     }
 
     @Test
@@ -81,7 +81,7 @@ public class ExpressoesCondicionaisTest extends EntityManagerTest {
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
         List<Object[]> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
     }
 
     @Test
@@ -92,6 +92,6 @@ public class ExpressoesCondicionaisTest extends EntityManagerTest {
         typedQuery.setParameter("nome", "a");
 
         List<Object[]> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
     }
 }
