6ff500bb7ee058e7ef284a9eccf6890bbf156c1b
AlsGomes <als_08.net@hotmail.com>
2023-06-06 18:46:44 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 09.10-exercicio-buscar-pedidos-com-produto-especifico/src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ 09.10-exercicio-buscar-pedidos-com-produto-especifico/src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -4,7 +4,9 @@ import com.algaworks.ecommerce.listener.GenericoListener;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
+import org.hibernate.Length;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -28,11 +30,13 @@ public class Produto extends EntidadeBaseInteger {
     private String nome;
 
     @Lob
+    @Column(length = Length.LONG32)
     private String descricao;
 
     private BigDecimal preco;
 
     @Lob
+    @Column(length = 1000)
     private byte[] foto;
 
     @ManyToMany
