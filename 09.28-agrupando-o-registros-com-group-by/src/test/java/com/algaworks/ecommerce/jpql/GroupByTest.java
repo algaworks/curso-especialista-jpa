2170c2fff8d9ef13bd1dc12a2faa4a1564a0a975
AlsGomes <als_08.net@hotmail.com>
2023-06-07 14:55:02 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 09.28-agrupando-o-registros-com-group-by/src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
+++ 09.28-agrupando-o-registros-com-group-by/src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -2,10 +2,10 @@ package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.StatusPedido;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import javax.persistence.TypedQuery;
+import jakarta.persistence.TypedQuery;
 import java.util.List;
 
 public class GroupByTest extends EntityManagerTest {
@@ -30,7 +30,7 @@ public class GroupByTest extends EntityManagerTest {
 
         List<Object[]> lista = typedQuery.getResultList();
 
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
 
         lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
     }
