626851d646fa7f1257d030ef4f60931791e46514
AlsGomes <als_08.net@hotmail.com>
2023-06-12 13:58:11 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 09.36-subqueries-com-in/src/test/java/com/algaworks/ecommerce/jpql/SubqueriesTest.java
+++ 09.36-subqueries-com-in/src/test/java/com/algaworks/ecommerce/jpql/SubqueriesTest.java
@@ -4,10 +4,10 @@ import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.Produto;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import javax.persistence.TypedQuery;
+import jakarta.persistence.TypedQuery;
 import java.util.List;
 
 public class SubqueriesTest extends EntityManagerTest {
@@ -21,7 +21,7 @@ public class SubqueriesTest extends EntityManagerTest {
         TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
 
         List<Pedido> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
 
         lista.forEach(obj -> System.out.println("ID: " + obj.getId()));
     }
@@ -47,7 +47,7 @@ public class SubqueriesTest extends EntityManagerTest {
         TypedQuery<Cliente> typedQuery = entityManager.createQuery(jpql, Cliente.class);
 
         List<Cliente> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
 
         lista.forEach(obj -> System.out.println("ID: " + obj.getId() + ", Nome: " + obj.getNome()));
     }
