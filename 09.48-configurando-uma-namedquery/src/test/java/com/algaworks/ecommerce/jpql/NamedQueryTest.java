c725cd3dfa9521abd4fdc5a09871055dc9889a3a
AlsGomes <als_08.net@hotmail.com>
2023-06-12 15:47:09 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 09.48-configurando-uma-namedquery/src/test/java/com/algaworks/ecommerce/jpql/NamedQueryTest.java
+++ 09.48-configurando-uma-namedquery/src/test/java/com/algaworks/ecommerce/jpql/NamedQueryTest.java
@@ -2,10 +2,10 @@ package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Produto;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import javax.persistence.TypedQuery;
+import jakarta.persistence.TypedQuery;
 import java.util.List;
 
 public class NamedQueryTest extends EntityManagerTest {
@@ -18,6 +18,6 @@ public class NamedQueryTest extends EntityManagerTest {
 
         List<Produto> lista = typedQuery.getResultList();
 
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
     }
 }
