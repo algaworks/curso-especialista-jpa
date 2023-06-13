e439108cdb9d45e1d3a94b31a66e3aafb4d79fc5
AlsGomes <als_08.net@hotmail.com>
2023-06-13 18:42:20 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 10.12-passando-parametros-para-as-consultas/src/main/java/com/algaworks/ecommerce/model/EntidadeBaseInteger.java
+++ 10.12-passando-parametros-para-as-consultas/src/main/java/com/algaworks/ecommerce/model/EntidadeBaseInteger.java
@@ -4,10 +4,10 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
 @Getter
 @Setter
