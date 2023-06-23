92a43fa5a2f70ae4010242c76557117fbaf7e090
AlsGomes <als_08.net@hotmail.com>
2023-06-23 17:09:17 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 10.39-subquery-relacionando-com-a-query-principal/src/test/java/com/algaworks/ecommerce/criteria/SubqueriesCriteriaTest.java
+++ 10.39-subquery-relacionando-com-a-query-principal/src/test/java/com/algaworks/ecommerce/criteria/SubqueriesCriteriaTest.java
@@ -2,14 +2,14 @@ package com.algaworks.ecommerce.criteria;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.*;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import javax.persistence.criteria.Subquery;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -38,7 +38,7 @@ public class SubqueriesCriteriaTest extends EntityManagerTest {
         TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
 
         List<Cliente> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
 
         lista.forEach(obj -> System.out.println(
                 "ID: " + obj.getId() + ", Nome: " + obj.getNome()));
@@ -65,7 +65,7 @@ public class SubqueriesCriteriaTest extends EntityManagerTest {
         TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
 
         List<Pedido> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
 
         lista.forEach(obj -> System.out.println(
                 "ID: " + obj.getId() + ", Total: " + obj.getTotal()));
@@ -92,7 +92,7 @@ public class SubqueriesCriteriaTest extends EntityManagerTest {
         TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
 
         List<Produto> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
 
         lista.forEach(obj -> System.out.println(
                 "ID: " + obj.getId() + ", Nome: " + obj.getNome() + ", Preço: " + obj.getPreco()));
