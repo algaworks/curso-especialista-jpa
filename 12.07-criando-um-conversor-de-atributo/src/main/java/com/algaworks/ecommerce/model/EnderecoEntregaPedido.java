641707926904fecca2773f56e678e397b11c207c
AlsGomes <als_08.net@hotmail.com>
2023-06-28 13:34:07 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 12.07-criando-um-conversor-de-atributo/src/main/java/com/algaworks/ecommerce/model/EnderecoEntregaPedido.java
+++ 12.07-criando-um-conversor-de-atributo/src/main/java/com/algaworks/ecommerce/model/EnderecoEntregaPedido.java
@@ -3,9 +3,9 @@ package com.algaworks.ecommerce.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.validation.constraints.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.*;
 
 @Getter
 @Setter
