ac728a552e8dd5d84a9f9631b46b5fca001e9397
AlsGomes <als_08.net@hotmail.com>
2023-07-04 17:40:09 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 13.05-modos-de-cache-e-a-anotacao-cacheable/src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
+++ 13.05-modos-de-cache-e-a-anotacao-cacheable/src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
@@ -4,8 +4,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
