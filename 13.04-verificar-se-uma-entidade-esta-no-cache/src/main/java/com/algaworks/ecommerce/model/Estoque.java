f376c0d6750196de2ea1e0863d5f87483ff0eda6
AlsGomes <als_08.net@hotmail.com>
2023-07-04 17:33:16 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 13.04-verificar-se-uma-entidade-esta-no-cache/src/main/java/com/algaworks/ecommerce/model/Estoque.java
+++ 13.04-verificar-se-uma-entidade-esta-no-cache/src/main/java/com/algaworks/ecommerce/model/Estoque.java
@@ -3,9 +3,9 @@ package com.algaworks.ecommerce.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 
 @Getter
 @Setter
--- 13.04-verificar-se-uma-entidade-esta-no-cache/src/main/java/com/algaworks/ecommerce/model/Pagamento.java
+++ 13.04-verificar-se-uma-entidade-esta-no-cache/src/main/java/com/algaworks/ecommerce/model/Pagamento.java
@@ -3,8 +3,8 @@ package com.algaworks.ecommerce.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @Setter
