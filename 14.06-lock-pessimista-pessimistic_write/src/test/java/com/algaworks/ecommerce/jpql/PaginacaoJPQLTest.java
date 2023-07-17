811356e6e98b7335084b8cb73a6c46f6eb490544
AlsGomes <als_08.net@hotmail.com>
2023-07-17 18:43:08 -0300

    Atualização para Hibernate 6, Java 17 e JUnit 5

--- 14.06-lock-pessimista-pessimistic_write/src/test/java/com/algaworks/ecommerce/jpql/PaginacaoJPQLTest.java
+++ 14.06-lock-pessimista-pessimistic_write/src/test/java/com/algaworks/ecommerce/jpql/PaginacaoJPQLTest.java
@@ -3,10 +3,10 @@ package com.algaworks.ecommerce.jpql;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Categoria;
 import com.algaworks.ecommerce.model.Pedido;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import javax.persistence.TypedQuery;
+import jakarta.persistence.TypedQuery;
 import java.util.List;
 
 public class PaginacaoJPQLTest extends EntityManagerTest {
@@ -22,7 +22,7 @@ public class PaginacaoJPQLTest extends EntityManagerTest {
         typedQuery.setMaxResults(2);
 
         List<Categoria> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Assertions.assertFalse(lista.isEmpty());
 
         lista.forEach(c -> System.out.println(c.getId() + ", " + c.getNome()));
     }
